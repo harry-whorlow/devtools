75475b9433c9f4641772884b68a0ba1de860ad5d
Alem Tuzlak <t.zlak@hotmail.com>
2025-07-30 09:20:51 +0200

    feat: make the plugin API better for react/solid

--- packages/react-devtools/src/devtools.tsx
+++ packages/react-devtools/src/devtools.tsx
@@ -1,10 +1,48 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { TanStackRouterDevtoolsCore } from '@tanstack/devtools'
-import type { DevtoolsOptions } from '@tanstack/devtools'
+import { createRoot } from 'react-dom/client'
+import type { JSX } from 'react'
+import type { DevtoolsOptions, DevtoolsPlugin } from '@tanstack/devtools'
 
-export const Devtools = (opts: DevtoolsOptions) => {
+type Render = JSX.Element | (() => JSX.Element)
+type ReactPlugin = Omit<DevtoolsPlugin, 'render' | 'name'> & {
+  render: Render
+  name: string | Render
+}
+interface DevtoolsProps {
+  plugins?: Array<ReactPlugin>
+  options?: DevtoolsOptions['options']
+}
+
+const convertRender = (
+  el: HTMLDivElement | HTMLHeadingElement,
+  Component: Render,
+) => {
+  const domNode = document.createElement('div')
+  const root = createRoot(domNode)
+  root.render(typeof Component === 'function' ? <Component /> : Component)
+  el.appendChild(domNode)
+}
+
+export const Devtools = ({ plugins, options }: DevtoolsProps) => {
   const devToolRef = useRef<HTMLDivElement>(null)
-  const [devtools] = useState(() => new TanStackRouterDevtoolsCore(opts))
+  const [devtools] = useState(
+    () =>
+      new TanStackRouterDevtoolsCore({
+        options,
+        plugins: plugins?.map((plugin) => {
+          return {
+            ...plugin,
+            name:
+              typeof plugin.name === 'string'
+                ? plugin.name
+                : // The check above confirms that `plugin.name` is of Render type
+                  (el) => convertRender(el, plugin.name as Render),
+            render: (el: HTMLDivElement) => convertRender(el, plugin.render),
+          }
+        }),
+      }),
+  )
   useEffect(() => {
     if (devToolRef.current) {
       devtools.mount(devToolRef.current)
--- packages/solid-devtools/src/devtools.tsx
+++ packages/solid-devtools/src/devtools.tsx
@@ -1,12 +1,44 @@
 import { TanStackRouterDevtoolsCore } from '@tanstack/devtools'
-import { createEffect, createSignal, onCleanup, onMount } from 'solid-js'
-import type { DevtoolsOptions } from '@tanstack/devtools'
+import { createEffect, createSignal, JSX, onCleanup, onMount } from 'solid-js'
+import type { DevtoolsOptions, DevtoolsPlugin } from '@tanstack/devtools'
+import { Portal } from 'solid-js/web'
+type Render = JSX.Element | (() => JSX.Element)
+const convertRender = (
+  el: HTMLDivElement | HTMLHeadingElement,
+  Component: Render,
+) => (
+  <Portal mount={el}>
+    {typeof Component === 'function' ? <Component /> : Component}
+  </Portal>
+)
 
-export const Devtools = (opts: DevtoolsOptions) => {
-  const [devtools] = createSignal(new TanStackRouterDevtoolsCore(opts))
+type SolidPlugin = Omit<DevtoolsPlugin, 'render' | 'name'> & {
+  render: Render
+  name: string | Render
+}
+interface DevtoolsProps {
+  plugins?: Array<SolidPlugin>
+  options?: DevtoolsOptions['options']
+}
+
+export const Devtools = ({ options, plugins }: DevtoolsProps) => {
+  const [devtools] = createSignal(
+    new TanStackRouterDevtoolsCore({
+      options,
+      plugins: plugins?.map((plugin) => ({
+        ...plugin,
+        name:
+          typeof plugin.name === 'string'
+            ? plugin.name
+            : // The check above confirms that `plugin.name` is of Render type
+              (el) => convertRender(el, plugin.name as Render),
+        render: (el: HTMLDivElement) => convertRender(el, plugin.render),
+      })),
+    }),
+  )
   let devToolRef: HTMLDivElement | undefined
   createEffect(() => {
-    devtools().setOptions(opts)
+    devtools().setOptions({ options })
   })
   onMount(() => {
     if (devToolRef) {
