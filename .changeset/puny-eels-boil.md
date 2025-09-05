6447ab20d6f479b9a71be2c727d70a441d09222b
Szymon Prusak <81956806+sprusak@users.noreply.github.com>
2025-09-05 17:29:36 +0200

    fix: safe build for target ssr (#109)
    - used tsup and new mapping as in query-devtools
    https://github.com/TanStack/query/blob/main/packages/query-devtools/package.json
    - eliminated browser only code for a server build, this makes sure
    projects with server side rendering can safely execute this code
    
    ---------
    
    Co-authored-by: Szymon Prusak <szymon.prusak@lastminute.com>

---
+++ .changeset/puny-eels-boil.md
@@ -0,0 +1,5 @@
+---
+'@tanstack/devtools': patch
+---
+
+Changed package.json exports to allow safe usage in ssr environments
--- packages/devtools/src/core.tsx
+++ packages/devtools/src/core.tsx
@@ -61,6 +61,9 @@ export class TanStackDevtoolsCore {
   }
 
   mount<T extends HTMLElement>(el: T) {
+    //  tsup-preset-solid statically replaces this variable during build, which eliminates this code from server bundle
+    if (import.meta.env.SSR) return
+
     if (this.#isMounted) {
       throw new Error('Devtools is already mounted')
     }
