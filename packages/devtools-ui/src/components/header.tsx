d33211fa8ba3513ebb152d41feac2649dd83c881
Harry Whorlow <whorlowharry@gmail.com>
2025-10-28 10:30:27 +0100

    feat: header click handler

--- packages/devtools-ui/src/components/header.tsx
+++ packages/devtools-ui/src/components/header.tsx
@@ -21,17 +21,19 @@ export function Header({
 export function HeaderLogo({
   children,
   flavor,
+  onClick,
 }: {
   children: JSX.Element
   flavor: {
     light: string
     dark: string
   }
+  onClick?: JSX.EventHandler<HTMLButtonElement, MouseEvent>
 }) {
   const styles = useStyles()
   return (
     <div class={styles().header.logoAndToggleContainer}>
-      <button class={clsx(styles().header.logo)}>
+      <button class={clsx(styles().header.logo)} onClick={onClick}>
         <span class={clsx(styles().header.tanstackLogo)}>TANSTACK</span>
         <span
           class={clsx(styles().header.flavorLogo(flavor.light, flavor.dark))}
