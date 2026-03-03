84c7c8452ee259847b595d05852ae030f3ccfc49
Harry Whorlow <whorlowharry@gmail.com>
2026-03-03 22:00:11 +0100

    chore: add test

--- packages/devtools-ui/tests/deep-keys.ts
+++ packages/devtools-ui/tests/deep-keys.ts
@@ -1,3 +1,6 @@
+import { describe, expectTypeOf, it } from 'vitest'
+
+// types
 import type { CollapsiblePaths } from '../src/utils/deep-keys'
 
 type WithDeeplyNestedObject = {
@@ -65,3 +68,55 @@ type WithGeneric<TData> = {
 }
 
 type _WithGeneric = CollapsiblePaths<WithGeneric<{ a: { b: string } }>>
+
+describe('deep-keys', () => {
+  it('should type deeply nested keys', () => {
+    expectTypeOf<_DeeplyNestedObject>().toEqualTypeOf<
+      | ''
+      | 'a'
+      | 'a.b'
+      | 'a.b.c'
+      | 'a.b.c.d'
+      | 'a.b.c.d.e'
+      | 'a.b.c.d.e.f'
+      | 'a.b.c.d.e.f.g'
+      | 'a.b.c.d.e.f.g.h'
+      | 'a.b.c.d.e.f.g.h.i'
+    >()
+  })
+
+  it('should handle any', () => {
+    expectTypeOf<_Any>().toEqualTypeOf<'' | 'errors'>()
+  })
+
+  it('should handle array recursion', () => {
+    expectTypeOf<_ArrayRecursion>().toEqualTypeOf<
+      | ''
+      | 'arr'
+      | `arr[${number}]`
+      | `arr[${number}][${number}]`
+      | `arr[${number}][${number}][${number}]`
+      | `arr[${number}][${number}][${number}][${number}]`
+    >()
+  })
+
+  it('should handle undefined', () => {
+    expectTypeOf<_WithUndefined>().toEqualTypeOf<
+      '' | 'status' | 'status.error'
+    >()
+  })
+
+  it('should handle unknown', () => {
+    expectTypeOf<_WithUnknown>().toEqualTypeOf<''>()
+  })
+
+  it('should handle realistic state', () => {
+    expectTypeOf<_WithRealisticState>().toEqualTypeOf<
+      '' | 'errors' | 'errorMap' | `errors[${number}]` | `errorMap.${string}`
+    >()
+  })
+
+  it('should handle generics', () => {
+    expectTypeOf<_WithGeneric>().toEqualTypeOf<'' | 'generic' | 'generic.a'>()
+  })
+})
