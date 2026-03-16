b0993b603a44bda166b4a4ccabfd5bfb41753ceb
Harry Whorlow <79278353+harry-whorlow@users.noreply.github.com>
2026-03-16 09:33:55 +0100

    fix(a11y): extract theme and fix pnpm overides (#392)

---
+++ .changeset/fuzzy-mice-hunt.md
@@ -0,0 +1,5 @@
+---
+'@tanstack/devtools-a11y': patch
+---
+
+Extract theme provider and fix pnpm overrides
--- packages/devtools-a11y/src/core/components/index.tsx
+++ packages/devtools-a11y/src/core/components/index.tsx
@@ -1,12 +1,20 @@
 /** @jsxImportSource solid-js */
-
+import { ThemeContextProvider } from '@tanstack/devtools-ui'
 import { AllyProvider } from '../contexts/allyContext'
 import { Shell } from './Shell'
 
-export default function Devtools() {
+import type { TanStackDevtoolsTheme } from '@tanstack/devtools-ui'
+
+interface DevtoolsProps {
+  theme: TanStackDevtoolsTheme
+}
+
+export default function Devtools(props: DevtoolsProps) {
   return (
-    <AllyProvider>
-      <Shell />
-    </AllyProvider>
+    <ThemeContextProvider theme={props.theme}>
+      <AllyProvider>
+        <Shell />
+      </AllyProvider>
+    </ThemeContextProvider>
   )
 }
